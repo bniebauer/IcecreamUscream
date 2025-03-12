600d14fb019bcd5c7283b3b93eb373916788a3d5
Brenton Niebauer <b.niebauer@me.com>
2025-03-12 17:00:55 -0400

    Initial Commit

---
+++ IcecreamUscream/IcecreamUscreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  IcecreamUscreamApp.swift
+//  IcecreamUscream
+//
+//  Created by Brenton Niebauer on 3/12/25.
+//
+
+import SwiftUI
+
+@main
+struct IcecreamUscreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
