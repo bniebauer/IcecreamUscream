f1cca19acac2c15c49791ded9eaf883d892a7af8
Brenton Niebauer <b.niebauer@me.com>
2025-03-28 20:27:18 -0400

    Add OrderView with QRCode logic

--- IcecreamUscream/OrderView.swift
+++ IcecreamUscream/OrderView.swift
@@ -5,11 +5,41 @@
 //  Created by Brenton Niebauer on 3/28/25.
 //
 
+import CoreImage.CIFilterBuiltins
 import SwiftUI
 
 struct OrderView: View {
+    @State private var qrCode = UIImage()
+    
+    let context = CIContext()
+    let filter = CIFilter.qrCodeGenerator()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Image(uiImage: qrCode)
+            .interpolation(.none)
+            .resizable()
+            .scaledToFit()
+    }
+    func updateQRCode() {
+//        let order = Order(id: UUID().uuidString, icecream: icecream, toppings: selectedToppings, scoops: numOfScoops)
+//
+//        if let data = try? JSONEncoder().encode(order) {
+//            let jsonString = String(data: data, encoding: .utf8)!
+//            qrCode = generateQRCode(from: jsonString)
+//        }
+    }
+    
+    func generateQRCode(from string: String) -> UIImage {
+        filter.message = Data(string.utf8)
+        
+        if let outputImage = filter.outputImage {
+            if let cgImage = context.createCGImage(outputImage, from: outputImage.extent) {
+                qrCode = UIImage(cgImage: cgImage)
+                return qrCode
+            }
+        }
+        
+        return UIImage(systemName: "xmark.circle") ?? UIImage()
     }
 }
 
