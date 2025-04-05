e9b8ec317d4017da730f0d71a6ba959dbabf7dca
Brenton Niebauer <b.niebauer@me.com>
2025-04-05 16:29:48 -0400

    add unit testing target

---
+++ IcecreamUscreamTests/IcecreamUscreamTests.swift
@@ -0,0 +1,17 @@
+//
+//  IcecreamUscreamTests.swift
+//  IcecreamUscreamTests
+//
+//  Created by Brenton Niebauer on 4/5/25.
+//
+
+import Testing
+@testable import IcecreamUscream
+
+struct IcecreamUscreamTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
